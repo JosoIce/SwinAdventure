899a14f2657c94ba0740af9a10bbb4a41efacd98
JosoIce <josoice@gmail.com>
2018-09-06 09:30:07 +1000

    First Commit

---
+++ SwinAdventureTests/IdentifiableObjectTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwinAdventure;
+
+namespace SwinAdventureTests
+{
+    [TestClass]
+    public class IdentifiableObjectTest
+    {
+        [TestMethod]
+        public void TestAreYou()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "id1", "id2", "id3" });
+
+            Assert.IsTrue(id.AreYou("id1"));
+        }
+
+        [TestMethod]
+        public void TestNotAreYou()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "id1", "id2", "id3" });
+
+            Assert.IsFalse(id.AreYou("id5"));
+        }
+
+        [TestMethod]
+        public void TestCaseSensitive()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "id1", "id2", "id3" });
+
+            Assert.IsTrue(id.AreYou("ID1"));
+        }
+
+        [TestMethod]
+        public void TestFirstId()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "id1", "id2", "id3" });
+            Assert.AreEqual(id.FirstId, "id1");
+        }
+
+        [TestMethod]
+        public void TestAddId()
+        {
+            IdentifiableObject id = new IdentifiableObject(new string[] { "id1", "id2", "id3" });
+
+            id.AddIdentifier("id4");
+
+            Assert.IsTrue(id.AreYou("id4"));
+        }
+    }
+}
---
+++ SwinAdventureTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SwinAdventureTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwinAdventureTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("09e4e293-5c7e-4665-ad9c-ed20a15e6e04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
