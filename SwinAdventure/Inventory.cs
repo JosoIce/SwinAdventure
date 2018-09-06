684b8fc82b71edfbd06fc223d0264cda18c8ca9e
JosoIce <josoice@gmail.com>
2018-09-06 12:20:56 +1000

    added new classes

---
+++ SwinAdventure/Inventory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    class Inventory
+    {
+        private List<Item> _items;
+
+        public Inventory()
+        {
+
+        }
+
+        public bool HasItem(string id)
+        {
+            foreach (Item i in _items)
+            {
+                if (i.FirstId == id) { return true;}
+            }
+
+            return false;
+        }
+
+        public void Put(Item itm)
+        {
+            _items.Add(itm);
+        }
+
+        public Item Take(string id)
+        {
+
+        }
+
+        public Item Fetch(string id)
+        {
+
+        }
+
+        public string ItemList
+        {
+            get
+            {
+                string itemOutput = "";
+                foreach (Item i in _items)
+                {
+                    itemOutput += i.ShortDescription + "\n";
+                }
+                return itemOutput;
+            }
+        }
+    }
+}
---
+++ SwinAdventure/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    class Player : GameObject
+    {
+        private Inventory _inventory;
+
+        public Player(string name, string desc) : base (new string[] { "me", "inventory" }, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public GameObject Locate(string id)
+        {
+            
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return "You are carrying:\n" + _inventory.ItemList;
+            }
+        }
+    }
+}
