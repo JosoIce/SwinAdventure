e2583a1fc21756d3c790db28d91538d19b974fa2
JosoIce <josoice@gmail.com>
2018-09-13 14:41:23 +1000

    added bag class

---
+++ SwinAdventure/Bag.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    public class Bag : Item
+    {
+        private Inventory _inventory;
+        public Bag(string[] ids, string name, string desc) : base (ids, name, desc)
+        {
+            
+        }
+
+        public GameObject Locate(string id)
+        {
+            if (AreYou(id))
+            {
+                return this;
+            }
+            return _inventory.Fetch(id);
+        }
+
+        public override string FullDescription
+        {
+            get
+            {
+                return "You are carrying:\n" + _inventory.ItemList;
+            }
+        }
+
+        public Inventory Inventory
+        {
+            get
+            {
+                return _inventory;
+            }
+        }
+    }
+}
---
+++ SwinAdventureTests/BagTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwinAdventureTests
+{
+    [TestClass]
+    public class BagTests
+    {
+        [TestMethod]
+        public void TestBagLocatesItems()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBagLocatesItself()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBagLocatesNothing()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBagFullDescription()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestBagInBag()
+        {
+
+        }
+    }
+}
