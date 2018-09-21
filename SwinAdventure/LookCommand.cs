84f637838e6ae10ead3b76e06a6c6e4d7e6d2d31
JosoIce <josoice@gmail.com>
2018-09-21 12:46:04 +1000

    Added base of Iteration 4

---
+++ SwinAdventure/LookCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventure
+{
+    public class LookCommand : Command
+    {
+        public LookCommand() : base(new string[] { "look" }) { }
+
+        public override string Excecute(Player p, string[] text)
+        {
+            
+        }
+
+        private IHaveInventory FetchContainer(Player p, string containerId)
+        {
+
+        }
+
+        private string LookAtIn(string thingId)
+    }
+}
